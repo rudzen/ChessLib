2e3c71e0e47899aa1f22e24d863cc985b1b7456e
Rudy Alex Kohn <s133235@student.dtu.dk>
2019-03-30 09:08:28 +0100

    Updated move notation generation to use better ambiguity type

---
+++ Chess/Enums/EMoveAmbiguity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rudz.Chess.Enums
+{
+    [Flags]
+    public enum EMoveAmbiguity : byte
+    {
+        None = 0,
+        File = 1,
+        Rank = 2
+    }
+
+    public static class EMoveAmbiguityExtensions
+    {
+        public static bool HasFlagFast(this EMoveAmbiguity value, EMoveAmbiguity flag)
+        {
+            return (value & flag) != 0;
+        }
+    }
+}
