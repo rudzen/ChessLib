2f1d10a3ab4f9305f529a22a9269ec5c78e36833
Rudy Alex Kohn <s133235@student.dtu.dk>
2019-03-30 10:14:05 +0100

    Added experimental movelist class

---
+++ Chess/MoveList.cs
@@ -0,0 +1,52 @@
+﻿namespace Rudz.Chess
+{
+    using Extensions;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using Types;
+
+    public sealed class MoveList : IEnumerable<Move>
+    {
+        private const int MaxPossibleMoves = 218;
+
+        private readonly Move[] _moves;
+
+        private int _moveIndex;
+
+        public MoveList(Move[] moves)
+        {
+            _moveIndex = -1;
+            _moves = moves;
+        }
+
+        public MoveList()
+        {
+            _moveIndex = -1;
+            _moves = new Move[MaxPossibleMoves];
+        }
+
+        public int Count => _moveIndex + 1;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(Move move) => _moves[++_moveIndex] = move;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Move GetMove(int index) =>
+            index.InBetween(0, _moveIndex)
+                ? _moves[index]
+                : MoveExtensions.EmptyMove;
+
+        public Move this[int index] => GetMove(index);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public IEnumerator<Move> GetEnumerator()
+        {
+            for (int i = 0; i < _moveIndex; ++i)
+                yield return _moves[i];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
