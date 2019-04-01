622c25d06787646f92ee3858c58e870696a57ebd
Rudy Alex Kohn <s133235@student.dtu.dk>
2019-04-01 06:22:51 +0200

    Added some groundwork for new ExtMove + Score structures

---
+++ Chess/Types/ExtMove.cs
@@ -0,0 +1,19 @@
+﻿namespace Rudz.Chess.Types
+{
+    /// <summary>
+    /// Extended move structure which combines Move and Score
+    /// </summary>
+    public struct ExtMove
+    {
+
+        public Move Move;
+
+        public Score Score;
+
+        public ExtMove(Move m, int s)
+        {
+            Move = m;
+            Score = s;
+        }
+    }
+}
