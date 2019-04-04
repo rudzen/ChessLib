26cf6056cbae77221069236c8e28721ffe7b802a
Rudy Alex Kohn <s133235@student.dtu.dk>
2019-04-04 21:08:22 +0200

    ExtMove "complete" + refined Score test

--- Chess/Types/ExtMove.cs
+++ Chess/Types/ExtMove.cs
@@ -1,19 +1,52 @@
-﻿namespace Rudz.Chess.Types
+﻿/*
+ChessLib, a chess data structure library
+
+MIT License
+
+Copyright (c) 2017-2019 Rudy Alex Kohn
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace Rudz.Chess.Types
 {
+    using System.Runtime.CompilerServices;
+
     /// <summary>
     /// Extended move structure which combines Move and Score
     /// </summary>
     public struct ExtMove
     {
-
         public Move Move;
 
         public Score Score;
 
-        public ExtMove(Move m, int s)
+        public ExtMove(Move m, Score s)
         {
             Move = m;
             Score = s;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ExtMove(Move m) => new ExtMove(m, 0);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ExtMove(Score s) => new ExtMove(MoveExtensions.EmptyMove, s);
     }
 }
