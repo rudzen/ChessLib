08d6ff2f735203e325355739306e96aae83f5a4d
Rudy Alex Kohn <s133235@student.dtu.dk>
2019-09-24 17:34:57 +0200

    added test version of new perft class

---
+++ Perft/PerftV2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Chess.Perft.Interfaces;
+using Perft.Parsers;
+
+namespace Perft
+{
+    public sealed class PerftV2
+    {
+        public PerftV2(IEpdParser parser)
+        {
+            Parser = parser;
+        }
+
+        public IEpdParser Parser { get; set; }
+
+        public Task<int> Run() => Run(CancellationToken.None);
+
+        public Task<int> Run(CancellationToken cancellationToken) => InternalRun(cancellationToken);
+
+        private async Task<int> InternalRun(CancellationToken cancellationToken)
+        {
+            return 0;
+        }
+
+        private static IEnumerable<IPerft> GeneratePerft(IEnumerable<IEpdSet> sets)
+        {
+            if (!sets.Any())
+                return Enumerable.Empty<IPerft>();
+
+            return Enumerable.Empty<IPerft>();
+        }
+
+
+    }
+}
