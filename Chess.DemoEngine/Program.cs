688c60b011dcd1428cd5322d4b069a9594a7db0f
Rudy Alex Kohn <rudzen@gmail.com>
2022-06-11 20:23:13 +0200

    Added missing main() to chess demo engine project

---
+++ Chess.DemoEngine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chess.DemoEngine
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine();
+            return 0;
+        }
+    }
+}
