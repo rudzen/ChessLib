ecac9460fc5844073a18f2f67041e9af53c0bcd7
Rudy Alex Kohn <rudzen@gmail.com>
2022-08-17 17:55:55 +0200

    Added implicit square operator from string (no validation check)

---
+++ Chess.Test/SquareTests/SquareFromStringTests.cs
@@ -0,0 +1,19 @@
+﻿using Rudz.Chess.Enums;
+using Rudz.Chess.Types;
+using Xunit;
+
+namespace Chess.Test.SquareTests;
+
+public class SquareFromStringTests
+{
+    [Fact]
+    public void SquareFromStringIsValid()
+    {
+        const string squareString = "e2";
+
+        var expected = new Square(Squares.e2);
+        Square actual = squareString;
+
+        Assert.Equal(expected, actual);
+    }
+}
