5613932d0c4e8ad18cb1264320026f87b62cb79f
Rudy Alex Kohn <rudzen@gmail.com>
2022-09-02 06:27:20 +0200

    Update perft unit tests

--- Chess.Test/MoveTests/PerftTest.cs
+++ Chess.Test/MoveTests/PerftTest.cs
@@ -24,137 +24,80 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Chess.Perft;
-using Chess.Perft.Interfaces;
+using Rudz.Chess.Factories;
 using Rudz.Chess.Fen;
 
 namespace Chess.Test.MoveTests;
 
-public sealed class PerftTest
+public sealed class PerftOneTest
 {
-    private const int ShortCount = 2;
+    [Theory]
+    [InlineData(Fen.StartPositionFen, 1, 20)]
+    [InlineData(Fen.StartPositionFen, 2, 400)]
+    [InlineData(Fen.StartPositionFen, 3, 8902)]
+    [InlineData(Fen.StartPositionFen, 4, 197281)]
+    [InlineData(Fen.StartPositionFen, 5, 4865609)]
+    [InlineData(Fen.StartPositionFen, 6, 119060324)]
+    public void Perft(string fen, int depth, ulong expected)
+        => PerftVerify.AssertPerft(fen, depth, in expected);
+}
 
-    private const int MediumCount = 4;
+public sealed class PerftTwoTest
+{
+    [Theory]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 1, 48)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2, 2039)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3, 97862)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, 4085603)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5, 193690690)]
+    public void Perft(string fen, int depth, ulong expected)
+        => PerftVerify.AssertPerft(fen, depth, in expected);
+}
 
-    private static readonly List<IPerftPosition> Positions = new(5)
+public sealed class PerftThreeTest
+{
+    [Theory]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 1, 14)]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 2, 191)]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 3, 2812)]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4, 43238)]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 5, 674624)]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 6, 11030083)]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 7, 178633661)]
+    public void Perft(string fen, int depth, ulong expected)
+        => PerftVerify.AssertPerft(fen, depth, in expected);
+}
+
+public sealed class PerftFourTest
+{
+    [Theory]
+    [InlineData("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 1, 6)]
+    [InlineData("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 2, 264)]
+    [InlineData("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 3, 9467)]
+    [InlineData("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 4, 422333)]
+    [InlineData("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 5, 15833292)]
+    public void Perft(string fen, int depth, ulong expected)
+        => PerftVerify.AssertPerft(fen, depth, in expected);
+}
+
+public sealed class PerftFiveTest
+{
+    [Theory]
+    [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 1, 46)]
+    [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 2, 2079)]
+    [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 3, 89890)]
+    [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, 3894594)]
+    [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 5, 164075551)]
+    public void Perft(string fen, int depth, ulong expected)
+        => PerftVerify.AssertPerft(fen, depth, in expected);
+}
+
+internal static class PerftVerify
+{
+    public static void AssertPerft(string fen, int depth, in ulong expected)
     {
-        PerftPositionFactory.Create(
-            Guid.NewGuid().ToString(),
-            Fen.StartPositionFen,
-            new List<(int, ulong)>(6)
-            {
-                (1, 20),
-                (2, 400),
-                (3, 8902),
-                (4, 197281),
-                (5, 4865609),
-                (6, 119060324)
-            }),
-        PerftPositionFactory.Create(
-            Guid.NewGuid().ToString(),
-            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-            new List<(int, ulong)>(6)
-            {
-                (1, 48),
-                (2, 2039),
-                (3, 97862),
-                (4, 4085603),
-                (5, 193690690),
-                (6, 8031647685)
-            }),
-        PerftPositionFactory.Create(
-            Guid.NewGuid().ToString(),
-            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
-            new List<(int, ulong)>(6)
-            {
-                (1, 14),
-                (2, 191),
-                (3, 2812),
-                (4, 43238),
-                (5, 674624),
-                (6, 11030083),
-                (7, 178633661)
-            }),
-        PerftPositionFactory.Create(
-            Guid.NewGuid().ToString(),
-            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-            new List<(int, ulong)>(6)
-            {
-                (1, 6),
-                (2, 264),
-                (3, 9467),
-                (4, 422333),
-                (5, 15833292),
-                (6, 706045033)
-            }),
-        PerftPositionFactory.Create(
-            Guid.NewGuid().ToString(),
-            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
-            new List<(int, ulong)>(6)
-            {
-                (1, 46),
-                (2, 2079),
-                (3, 89890),
-                (4, 3894594),
-                (5, 164075551),
-                (6, 6923051137)
-            })
-    };
-
-    [Fact]
-    public async Task PerftShort()
-    {
-        foreach (var perftPosition in Positions)
-        {
-            var perft = PerftFactory.Create();
-            perft.AddPosition(perftPosition);
-            var expected = perft.GetPositionCount(0, ShortCount);
-            var perftResults = perft.DoPerft(ShortCount);
-
-            var actual = ulong.MinValue;
-            await foreach (var result in perftResults.ConfigureAwait(false))
-                actual += result;
-
-            Assert.Equal(expected, actual);
-        }
-    }
-
-    [Fact]
-    public async Task PerftMedium()
-    {
-        foreach (var perftPosition in Positions)
-        {
-            var perft = PerftFactory.Create();
-            perft.AddPosition(perftPosition);
-            var expected = perft.GetPositionCount(0, MediumCount);
-            var perftResults = perft.DoPerft(MediumCount);
-
-            var actual = ulong.MinValue;
-            await foreach (var result in perftResults.ConfigureAwait(false))
-                actual += result;
-
-            Assert.Equal(expected, actual);
-        }
-    }
-
-    [Fact]
-    public async Task PerftFull()
-    {
-        foreach (var perftPosition in Positions)
-        {
-            var perft = PerftFactory.Create();
-            perft.AddPosition(perftPosition);
-            var expected = perft.GetPositionCount(0, Positions.Count);
-            var perftResults = perft.DoPerft(Positions.Count);
-
-            var actual = ulong.MinValue;
-            await foreach (var result in perftResults.ConfigureAwait(false))
-                actual += result;
-
-            Assert.Equal(expected, actual);
-        }
+        var g = GameFactory.Create(fen);
+        var actual = g.Perft(depth, true);
+        Assert.Equal(expected, actual);
     }
 }
