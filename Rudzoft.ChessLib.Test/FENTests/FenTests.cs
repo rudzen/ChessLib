7a1d56b703f64f38795e0f9b97906910f6f22fe5
Rudy Alex Kohn <rudzen@gmail.com>
2022-09-04 12:01:56 +0200

    Improved Fen tests + Move generator tests

--- Rudzoft.ChessLib.Test/FENTests/FenTests.cs
+++ Rudzoft.ChessLib.Test/FENTests/FenTests.cs
@@ -32,19 +32,18 @@ namespace Rudzoft.ChessLib.Test.FENTests;
 
 public sealed class FenTests
 {
-    [Fact]
-    public void GetFen()
+    [Theory]
+    [InlineData(Fen.Fen.StartPositionFen)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")]
+    [InlineData("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1")]
+    [InlineData("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")]
+    [InlineData("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10")]
+    [InlineData("rnbqk2r/pppppppp/8/8/8/8/PPPPPPPP/RNBQK2R w KQkq - 5 25")]
+    [InlineData("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1")]
+    public void GetFen(string fen)
     {
-        var board = new Board();
-        var pieceValue = new PieceValue();
-        var pos = new Position(board, pieceValue);
-        var game = GameFactory.Create(pos);
-        game.NewGame();
-
-        var expectedFen = new FenData(Fen.Fen.StartPositionFen);
-
-        var actualFen = game.GetFen();
-
-        Assert.Equal(expectedFen, actualFen);
+        var g = GameFactory.Create(fen);
+        var actualFen = g.GetFen().ToString();
+        Assert.Equal(fen, actualFen);
     }
 }
