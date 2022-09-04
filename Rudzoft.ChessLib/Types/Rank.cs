1f2bbfccf173e3521920e74ebde2bb2fb060e6a1
Rudy Alex Kohn <rudzen@gmail.com>
2022-09-04 12:00:19 +0200

    Improved ExtMove, Player & Rank

--- Rudzoft.ChessLib/Types/Rank.cs
+++ Rudzoft.ChessLib/Types/Rank.cs
@@ -65,6 +65,8 @@ public readonly record struct Rank(Ranks Value)
 
     public static Rank[] PawnRanks { get; } = { RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7 };
 
+    public static Rank[] AllRanks { get; } = { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8 };
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Rank(int rank) : this((Ranks)rank)
     {
