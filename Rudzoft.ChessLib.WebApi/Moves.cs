c1757ba1c1ea541826b266c1b77d803062bd39a1
Rudy Alex Kohn <rudzen@gmail.com>
2022-09-04 13:45:11 +0200

    Added very basic web api for example usage

---
+++ Rudzoft.ChessLib.WebApi/Moves.cs
@@ -0,0 +1,4 @@
+
+namespace Rudzoft.ChessLib.WebApi;
+
+public record Moves(IList<string> MoveList, int Size);
---
+++ Rudzoft.ChessLib.WebApi/Services/IMoveGeneratorService.cs
@@ -0,0 +1,8 @@
+﻿using Rudzoft.ChessLib.WebApi.Queries;
+
+namespace Rudzoft.ChessLib.WebApi.Services;
+
+public interface IMoveGeneratorService
+{
+    IEnumerable<string> GenerateMoves(MoveQuery parameters);
+}
