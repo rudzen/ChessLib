03bfd67cfeccc3a714752e8112928534809c44e0
Rudy Alex Kohn <rudzen@gmail.com>
2022-09-04 13:45:11 +0200

    Added very basic web api for example usage

---
+++ Rudzoft.ChessLib.WebApi/Controllers/MovesController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Rudzoft.ChessLib.WebApi.Queries;
+using Rudzoft.ChessLib.WebApi.Services;
+
+namespace Rudzoft.ChessLib.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovesController : ControllerBase
+{
+    private readonly ILogger<MovesController> _logger;
+    private readonly IMoveGeneratorService _moveGeneratorService;
+
+    public MovesController(ILogger<MovesController> logger, IMoveGeneratorService moveGeneratorService)
+    {
+        _logger = logger;
+        _moveGeneratorService = moveGeneratorService;
+    }
+
+    [HttpGet(Name = "generate")]
+    public IActionResult Get([FromQuery] MoveQuery parameters)
+    {
+        var m = _moveGeneratorService.GenerateMoves(parameters).ToList();
+        _logger.LogInformation("Moves fetched. size={0}", m.Count);
+        return Ok(new Moves(m, m.Count));
+    }
+}
---
+++ Rudzoft.ChessLib.WebApi/Queries/MoveQuery.cs
@@ -0,0 +1,5 @@
+﻿using Rudzoft.ChessLib.Enums;
+
+namespace Rudzoft.ChessLib.WebApi.Queries;
+
+public sealed record MoveQuery(string Fen = Fen.Fen.StartPositionFen, MoveGenerationType Type = MoveGenerationType.Legal);
