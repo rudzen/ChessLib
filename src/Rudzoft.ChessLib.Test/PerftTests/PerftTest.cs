e4583c3896f7edf34bf0deaa37929778ed0f5acc
Rudy Alex Kohn <rudzen@gmail.com>
2024-03-04 23:05:50 +0100

    added Peter Jones' perft tests

--- src/Rudzoft.ChessLib.Test/PerftTests/PerftTest.cs
+++ src/Rudzoft.ChessLib.Test/PerftTests/PerftTest.cs
@@ -210,3 +210,55 @@ public sealed class WeirdErrorFenTest : PerftVerify
     public void Perft(string fen, int depth, ulong expected)
         => AssertPerft(fen, depth, in expected);
 }
+
+/// <summary>
+/// https://gist.github.com/peterellisjones/8c46c28141c162d1d8a0f0badbc9cff9
+/// </summary>
+public sealed class PeterJonesPerftTests : PerftVerify
+{
+    private static readonly string[] Fens =
+    [
+        "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2",
+        "8/8/8/2k5/2pP4/8/B7/4K3 b - d3 0 3",
+        "r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2",
+        "r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2",
+        "2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2",
+        "rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9",
+        "2r5/3pk3/8/2P5/8/2K5/8/8 w - - 5 4",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1",
+        "8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1",
+        "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1",
+        "5k2/8/8/8/8/8/8/4K2R w K - 0 1",
+        "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1",
+        "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1",
+        "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1",
+        "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1",
+        "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1",
+        "4k3/1P6/8/8/8/8/K7/8 w - - 0 1",
+        "8/P1k5/K7/8/8/8/8/8 w - - 0 1",
+        "K1k5/8/P7/8/8/8/8/8 w - - 0 1",
+        "8/k1P5/8/1K6/8/8/8/8 w - - 0 1",
+        "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1"
+    ];
+
+    private static readonly int[] Depths =
+    [
+        1, 1, 1, 1, 1, 1, 1, 3, 3, 6, 6, 6, 6, 6, 4, 4, 6,
+        5, 6, 6, 6, 7, 4
+    ];
+
+    private static readonly ulong[] Results =
+    [
+        8, 8, 19, 5, 44, 39, 9, 62379, 89890, 1134888, 1015133, 1440467, 661072, 803711, 1274206, 1720476, 3821001,
+        1004658, 217342, 92683, 2217, 567584, 23527
+    ];
+
+    public static readonly PerftTheoryData PerftTheoryData = new(Fens, Depths, Results);
+
+    [Theory]
+    [MemberData(nameof(PerftTheoryData))]
+    public void Perft(string fen, int depth, ulong expected)
+        => AssertPerft(fen, depth, in expected);
+}
